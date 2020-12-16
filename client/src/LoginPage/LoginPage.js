db31e0d43ef1ee2fe13135eed796ad8d874c8fe8
Andrey-Roshchupkin <73870254+Andrey-Roshchupkin@users.noreply.github.com>
2020-12-16 20:28:52 +0500

    Two links added

--- client/src/LoginPage/LoginPage.js
+++ client/src/LoginPage/LoginPage.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
+
 export const LoginPage = () => {
     return(
     <>
       <p>Страница логина</p>
+
+      <Link to="/registration">Зарегистрироваться</Link>
     </>
   );
 };
--- client/src/RegistrationPage/RegistrationPage.js
+++ client/src/RegistrationPage/RegistrationPage.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export const RegistrationPage = () => {
   return (
     <>
       <p>Страница регистрации</p>
+      <Link to="/login">Авторизоваться</Link>
     </>
   );
 };
